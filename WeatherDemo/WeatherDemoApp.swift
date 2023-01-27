484b8433c2a09468332f21a9846004c0f7ce6626
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2023-01-27 02:18:47 -0300

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Guilherme Golfetto on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
